c2f866e07e3d37d9e97c7d2e4d7dd88eca326a76
Gilyana Saran <vld06@ukr.net>
2017-04-18 18:49:57 +0300

    Исправил трэвиса 1

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//1
+// 1
 function makeRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
@@ -23,38 +23,38 @@ function makeRandomY() {
   return Math.floor(Math.random() * 100) + 500;
 }
 
-var titleArr = ["Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде"];
-var typeArr = ["flat", "house", "bungalo"];
-var checkArr = ["12:00", "13:00", "14:00"];
-var featuresArr = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+var titleArr = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var typeArr = ['flat', 'house', 'bungalo'];
+var checkArr = ['12:00', '13:00', '14:00'];
+var featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
 var pinArr = [];
 for (var i = 0; i < 8; i++) {
   var apartObj = {
     "author": {
-      "avatar": ""
+      "avatar": ''
     },
     "offer": {
-      "title": "",
-      "address": "",
-      "price": "",
-      "type": "",
-      "rooms": "",
-      "guests": "",
-      "checkin": "",
-      "checkout": "",
-      "features": "",
-      "description": "",
-      "photos": ""
+      "title": '',
+      "address": '',
+      "price": '',
+      "type": '',
+      "rooms": '',
+      "guests": '',
+      "checkin": '',
+      "checkout": '',
+      "features": '',
+      "description": '',
+      "photos": ''
     },
     "location": {
-      "x": "",
-      "y": ""
+      "x": '',
+      "y": ''
     }
   };
-  apartObj.author.avatar = "img/avatars/user0" + makeRandomAvatar() + ".png";
+  apartObj.author.avatar = 'img/avatars/user0' + makeRandomAvatar() + '.png';
   apartObj.offer.title = makeRandom(titleArr);
-  apartObj.offer.address = "{{location.x}}, {{location.y}}";
+  apartObj.offer.address = '{{location.x}}, {{location.y}}';
   apartObj.offer.price = makeRandomPrice();
   apartObj.offer.type = makeRandom(typeArr);
   apartObj.offer.rooms = makeRandomRooms();
@@ -69,7 +69,7 @@ for (var i = 0; i < 8; i++) {
 }
 
 
-//2
+// 2
 var tokyoPinMap = document.querySelector('.tokyo__pin-map');
 var pinElement = document.createElement('div');
 pinElement.className = 'pin';
@@ -81,20 +81,17 @@ var imgElement = pinElement.getElementsByTagName('img')[0];
 imgElement.className = 'rounded';
 
 imgElement.src = apartObj.author.avatar;
-imgElement.width = "40";
-imgElement.height = "40";
+imgElement.width = '40';
+imgElement.height = '40';
 
-/*
-//3
+// 3
 var fragment = document.createDocumentFragment();
 var firstTokyo = document.querySelector(".tokyo__pin-map");
 
 fragment.appendChild(pinElement);
 firstTokyo.appendChild(fragment);
-*/
 
-
-//4
+// 4
 var similarLodgeTemplate = document.querySelector('#lodge-template').content;
 var similarDialogPanelEl = document.querySelector('.dialog__panel');
 var hotelElement = similarLodgeTemplate.cloneNode(true);
@@ -102,15 +99,15 @@ var hotelElement = similarLodgeTemplate.cloneNode(true);
 hotelElement.querySelector('.lodge__title').textContent = pinArr[0].offer.title;
 hotelElement.querySelector('.lodge__address').textContent = pinArr[0].offer.address;
 hotelElement.querySelector('.lodge__price').textContent = pinArr[0].offer.address + '&#x20bd;/ночь';
-var offerTypeEl = pinArr[0].offer.type
+var offerTypeEl = pinArr[0].offer.type;
 if (offerTypeEl === 'flat') {
   hotelElement.querySelector('.lodge__type').textContent = 'Квартира'
-}
+};
 if (offerTypeEl === 'bungalo') {
-  hotelElement.querySelector('.lodge__type').textContent = 'Бунгало'
+  hotelElement.querySelector('.lodge__type').textContent = 'Бунгало';
 }
 if (offerTypeEl === 'house') {
-  hotelElement.querySelector('.lodge__type').textContent = 'Дом '
+  hotelElement.querySelector('.lodge__type').textContent = 'Дом';
 }
 
 hotelElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + pinArr[0].offer.guests + ' гостей в ' + pinArr[0].offer.rooms + ' комнатах';
